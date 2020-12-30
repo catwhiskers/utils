f4fed2f1a2faa47f7e640cd4560690be94f35739
EC2 Default User <ec2-user@ip-172-16-187-30.ec2.internal>
2020-12-30 00:56:16 +0000

    add authentication

--- elastic_search/esquery.py
+++ elastic_search/esquery.py
@@ -1,9 +1,21 @@
+import boto3
 import requests
+from requests_aws4auth import AWS4Auth
+
+def get_awsauth(elastic_search_endpoint):
+    host = elastic_search_endpoint
+    session = boto3.session.Session()
+    region = session.region_name
+    service = 'es'
+    credentials = boto3.Session().get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+    return awsauth
 
 
-def query(query, field, elastic_search_endpoint, master_user, master_user_password):
+
+def query(elastic_search_endpoint, query, field, awsauth):
     r = requests.get('https://{}/product/product/_search?q={}:{}&size=100'.format(elastic_search_endpoint, field, query)
-                     ,auth=(master_user, master_user_password))
+                     ,auth=awsauth)
     rjson = r.json()
     return rjson
 
